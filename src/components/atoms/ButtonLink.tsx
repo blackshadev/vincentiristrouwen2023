f31aa5e362d8173ade367dbec97f414438bf87fd
Vincent Hagen <vinnie@script4web.nl>
2023-02-03 22:09:39 +0100

    Initial commit!

---
+++ src/components/atoms/ButtonLink.tsx
@@ -0,0 +1,12 @@
+import classNames from '@/helpers/classNames';
+import { PropsWithChildren } from 'react';
+type Props = React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+export default function ButtonLink({children, className, ...props}: PropsWithChildren<Props>) {
+    return <a 
+        className={classNames(
+            "mt-6 inline-block rounded-md bg-purple-600 px-4 py-2 text-base font-semibold leading-7 text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600",
+            className
+        )}
+        {...props}
+    >{children}</a>;
+}
---
+++ src/helpers/classNames.ts
@@ -0,0 +1,4 @@
+export default function classNames(...args: (string|false|null|undefined)[]): string
+{
+    return args.filter((value) => !!value).join(' ');
+}
