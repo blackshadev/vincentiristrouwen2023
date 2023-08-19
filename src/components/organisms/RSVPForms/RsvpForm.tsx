9f5b7d45ba76f5039fc9c10b9ab0fbc31226b27b
Vincent Hagen <vinnie@script4web.nl>
2023-08-19 11:07:58 +0200

    Remove R.S.V.P Form

--- src/components/organisms/RSVPForms/RsvpForm.tsx
+++ src/components/organisms/RSVPForms/RsvpForm.tsx
@@ -14,7 +14,12 @@ export default function RsvpForm({ className, type }: Props) {
     <section className={classNames(className, "grid gap-4 md:grid-cols-2")}>
       <div>
         <SectionTitle className="text-center mb-2">R. S. V. P.</SectionTitle>
-        <p className="text-center mb-4 text-gray-500 font-bold">Laat ons weten of je erbij bent!</p>
+        <p className="text-center mb-4 text-gray-500 font-bold">Het is bijna zo ver!</p>
+        <p className="mb-2">
+          Het R.S.V.P. formulier is gesloten. Wil je last minute wijzingen door te geven? Dan kan je dit aan Iris of Vincent doorgeven.
+        </p>
+      </div>
+        {/* <p className="text-center mb-4 text-gray-500 font-bold">Laat ons weten of je erbij bent!</p>
         <p className="mb-2">
           Wij vinden het fijn om te weten of we op je kunnen rekenen. In het naam veld graag maar één naam invullen. 
           Als je met meer dan één persoon komt kan je dit formulier vaker invullen.
@@ -23,7 +28,7 @@ export default function RsvpForm({ className, type }: Props) {
           <p className="mb-2">Geef ook direct je menu keuzen en eventuele allergiën door.</p>
         )}
       </div>
-      <Form type={type} />
+      <Form type={type} /> */}
     </section>
   );
 }
